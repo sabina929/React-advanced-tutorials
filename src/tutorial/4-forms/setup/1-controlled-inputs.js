fceb6ba1cdbf7d2558ad3ca76ecbcf45d508a797
Sabina <sabina.abbasova2909@gmail.com>
2021-03-29 19:16:43 +0400

    forms one

--- src/tutorial/4-forms/setup/1-controlled-inputs.js
+++ src/tutorial/4-forms/setup/1-controlled-inputs.js
@@ -8,11 +8,23 @@ import React, { useState } from 'react';
 const ControlledInputs = () => {
   const [firstName, setFirstName] = useState('')
   const [email, setEmail] = useState('')
+  const [people, setPeople] = useState([])
 
 
   const handleSubmit = (e) => {
     e.preventDefault()
-    console.log(firstName, email)
+    // console.log(firstName, email)
+
+    if(firstName && email){
+      const person = {id:new Date().getTime().toString(), firstName, email}
+      setPeople((prevPeople) => {
+        return [...prevPeople, person]
+      })
+      setFirstName('')
+      setEmail('')
+    } else{
+      console.log('empty values')
+    }
   }
 
   return (
@@ -41,6 +53,17 @@ const ControlledInputs = () => {
           </div>
           <button type='submit'>add person</button>
         </form>
+
+        {people.map((person) => {
+          const {id, firstName, email} = person
+
+          return (
+            <div className="item" key={id}>
+              <h4>{firstName}</h4>
+              <p>{email}</p>
+            </div>
+          )
+        })}
  
       </article>
     </>
