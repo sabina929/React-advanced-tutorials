b8ab804f3cf27706fe2b049e45ac86034634d1d9
Sabina <sabina.abbasova2909@gmail.com>
2021-03-31 19:43:57 +0400

    prop types

--- src/App.js
+++ src/App.js
@@ -24,7 +24,9 @@ import React from 'react'
 
 // import Setup from './tutorial/8-useContext/setup/1-context-api'
 
-import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example'
+// import Setup from './tutorial/9-custom-hooks/setup/1-fetch-example'
+
+import Setup from './tutorial/10-prop-types/setup'
 
 function App() {
   return (
