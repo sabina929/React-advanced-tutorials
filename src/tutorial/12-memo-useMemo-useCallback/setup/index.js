a1b3ee073e2739e4ead4e9895a342f71b2936257
Sabina <sabina.abbasova2909@gmail.com>
2021-04-02 23:37:12 +0400

    useMemo

--- src/tutorial/12-memo-useMemo-useCallback/setup/index.js
+++ src/tutorial/12-memo-useMemo-useCallback/setup/index.js
@@ -7,6 +7,23 @@ const url = 'https://course-api.com/javascript-store-products'
 
 // every time props or state changes, component re-renders
 
+// useCallback for function changings (addToCart)
+// React.memo for prop, state changings (count)
+// useMemo for value changings (products)
+
+const calculateMostExpensive = (data) => {
+  console.log('calculated')
+  return(
+    data.reduce((total,item)=>{
+      const price = item.fields.price
+      if(price>=total){
+        total = price
+      }
+      return total
+    }, 0) / 100
+  )
+}
+
 const Index = () => {
   const { products } = useFetch(url)
   const [count, setCount] = useState(0)
@@ -16,6 +33,7 @@ const Index = () => {
     setCart(cart+1)
   }, [cart])
 
+  const mostExpensive = useMemo(()=>calculateMostExpensive(products), [products])
   return (
     <>
       <h1>Count : {count}</h1>
@@ -23,6 +41,7 @@ const Index = () => {
         click me
       </button>
       <h1>Cart : {cart}</h1>
+      <h1>Most Expensive : ${mostExpensive}</h1>
       <BigList products={products} addToCart={addToCart} />
     </>
   )
