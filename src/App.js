1c2e6ba331966fc8afec2acedb2276f3b237c23e
Sabina <sabina.abbasova2909@gmail.com>
2021-04-02 23:16:01 +0400

    memoizing

--- src/App.js
+++ src/App.js
@@ -28,7 +28,9 @@ import React from 'react'
 
 // import Setup from './tutorial/10-prop-types/setup'
 
-import Setup from './tutorial/11-react-router/setup'
+// import Setup from './tutorial/11-react-router/setup'
+
+import Setup from './tutorial/12-memo-useMemo-useCallback/setup'
 
 function App() {
   return (
